7838f8b6d2a6c536fe5d9727880522a629307bdd
ANKIT KUMAR <136682826+ANKIT405@users.noreply.github.com>
2024-07-08 19:56:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
 let mongoose=require("mongoose")
+function connect(){
 mongoose.connect("mongodb://0.0.0.0:27017/Userdetails").then(()=>{
     console.log("Database connected")
 }).catch((err)=>{
   console.log("Problem occur", err)
 })
+}
+module.exports=connect
