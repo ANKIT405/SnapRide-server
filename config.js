1b3a216464e0ea5841a48f74624fcc79ffb0187f
ANKIT KUMAR <136682826+ANKIT405@users.noreply.github.com>
2024-07-13 12:56:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let mongoose=require("mongoose")
 function connect(){
-mongoose.connect("mongodb://0.0.0.0:27017/Userdetails",{
+mongoose.connect("mongodb://localhost:27017/Userdetails",{
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
