e079bdc55f78fe2f9c6f663c3ef9584239793ff0
ANKIT KUMAR <136682826+ANKIT405@users.noreply.github.com>
2024-07-13 12:48:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
 let mongoose=require("mongoose")
 function connect(){
-mongoose.connect("mongodb://0.0.0.0:27017/Userdetails").then(()=>{
+mongoose.connect("mongodb://0.0.0.0:27017/Userdetails",{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
+  socketTimeoutMS: 45000, // Increase socket timeout to 45 seconds
+}).then(()=>{
     console.log("Database connected")
 }).catch((err)=>{
   console.log("Problem occur", err)
