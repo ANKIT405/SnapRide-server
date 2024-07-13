3759d743a0c8614d354470108ac3da8ccd324d0e
ANKIT KUMAR <136682826+ANKIT405@users.noreply.github.com>
2024-07-13 13:55:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let mongoose=require("mongoose")
 function connect(){
-mongoose.connect('mongodb://ankit:ankit%40966@localhost:27017/Userdetails',{
+mongoose.connect('mongodb://ankit:ankit%40966@127.0.0.1:27017/Userdetails',{
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
