73c132881d1be86c4c003025d19ca87d02b85c1b
ANKIT KUMAR <136682826+ANKIT405@users.noreply.github.com>
2024-07-16 12:22:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let mongoose=require("mongoose")
 function connect(){
-mongoose.connect('mongodb+srv://Userdetails:q55FKZ6kBcVw4Btr@atlascluster.nn8yirt.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster').then(()=>{
+mongoose.connect('mongodb+srv://UserDatabase:r1rfy1o4Nl9CNIKo@userdb.pamhqez.mongodb.net/?retryWrites=true&w=majority&appName=UserDB').then(()=>{
     console.log("Database connected")
 }).catch((err)=>{
   console.log("Problem occur", err)
