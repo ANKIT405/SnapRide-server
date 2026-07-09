be6fbff5c886a9073be056cdafafe6754dce188f
ANKIT KUMAR <136682826+ANKIT405@users.noreply.github.com>
2026-07-09 20:12:46 +0530

    Update MongoDB connection string credentials

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let mongoose=require("mongoose")
 function connect(){
-mongoose.connect('mongodb+srv://UserDatabase:r1rfy1o4Nl9CNIKo@userdb.pamhqez.mongodb.net/?retryWrites=true&w=majority&appName=UserDB').then(()=>{
+mongoose.connect('mongodb+srv://UserDatabase:Ankit966@userdb.pamhqez.mongodb.net/?retryWrites=true&w=majority&appName=UserDB').then(()=>{
     console.log("Database connected")
 }).catch((err)=>{
   console.log("Problem occur", err)
